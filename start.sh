9f877eecd28d7413e09637a268f560a35e897d4e
sepiol <sepiol@kali-sepiol>
2023-08-10 19:43:50 +0200

    maj script

--- start.sh
+++ start.sh
@@ -1,4 +1,23 @@
 #! /bin/bash
+
+if ! command -v npm &> /dev/null
+then
+    echo "npm n'est pas installé. Veuillez l'installer avant de continuer."
+    exit 1
+fi
+
+if ! command -v docker &> /dev/null
+then
+    echo "docker n'est pas installé. Veuillez l'installer avant de continuer."
+    exit 1
+fi
+
+if ! command -v docker-compose &> /dev/null
+then
+    echo "docker-compose n'est pas installé. Veuillez l'installer avant de continuer."
+    exit 1
+fi
+
 cd app
 npm install
 cd ..
